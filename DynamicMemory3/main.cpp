9bc02652d69ded14b28da0dc38a22287e40b52d0
Anna <goryachevaanny@gmail.com>
2021-03-12 21:12:00 +0300

    Initial Commit

---
+++ DynamicMemory3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DynamicMemory3
+//
+//  Created by Анна Горячева on 12.03.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
